3fa45abe80904cc5f4de1bf752eb95e8c91b0785
Andrew Hinojosa <drewsview34@gmail.com>
2019-01-23 19:02:43 -0800

    Added Image

---
+++ Lab40111-MyFirstMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab40111_MyFirstMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //This Is The Index Page. Use This Block Of Code To Create New Pages.
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
